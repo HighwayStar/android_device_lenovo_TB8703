083c48e8ca72606cb18e00f417e2acd86819012b
Michael Bestas <mkbestas@lineageos.org>
2017-07-30 22:10:12 +0300

    TB8703: Initial device tree
    * Add basic stuff to compile TWRP under lineage
      including decryption support
    
    Change-Id: Ibf41103a9fd0f6af0938c42b2350d71381010a43

---
+++ lineage.mk
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1080
+TARGET_SCREEN_WIDTH := 1920
+
+# Inherit 64-bit configs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lenovo/TB8703/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_TB8703
+PRODUCT_DEVICE := TB8703
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := Lenovo TB-8703
+PRODUCT_MANUFACTURER := LENOVO
+
+PRODUCT_GMS_CLIENTID_BASE := android-lenovo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=Lenovo/TB-8703X/TB-8703X:6.0.1/MMB29M/TB-8703X_USR_S034_170525_Q1241_ROW:user/release-keys \
+    PRIVATE_BUILD_DESC="msm8953_64-user 6.0.1 MMB29M 477 release-keys" \
+    TARGET_DEVICE="TB8703"

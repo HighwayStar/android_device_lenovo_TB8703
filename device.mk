19ae772cee6c37cc5ff2c8aad3ab0a621634af8f
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:09:54 +0200

    tb8703: Build Lights HIDL HAL interface
    Change-Id: I6ce412afa476924d7c0bd270fe7ca341873d2680

--- device.mk
+++ device.mk
@@ -148,6 +148,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
 
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
+
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \

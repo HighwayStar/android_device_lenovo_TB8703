91312cc9d73915a8aa7e13a4da112d8c460f44de
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:17:09 +0200

    tb8703: Build Vibrator HIDL HAL interface
    Change-Id: I3353060afd4f7b63ebd94a3a46d40cb7e651d8b5

--- device.mk
+++ device.mk
@@ -277,6 +277,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libshims_camera
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
 # WiFi
 PRODUCT_PACKAGES += \
     libqsap_sdk \

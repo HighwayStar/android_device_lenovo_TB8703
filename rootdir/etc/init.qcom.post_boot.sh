2f270be12cd1ed7513157ed2e47161ccb03a9779
Manjunatha Ramachandra <mramac@codeaurora.org>
2017-09-13 12:08:47 +0530

    tb8703:  set sync wakee policy tunable
    Prefer waker CPU (with  1 runnable task) instead of
    an idle CPU in waker cluster.
    
    Change-Id: I174d496cf31bbf8706eaf6ad3740883d1c64b20f

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -1154,6 +1154,8 @@ case "$target" in
                 # Apply inter-cluster load balancer restrictions
                 echo 1 > /proc/sys/kernel/sched_restrict_cluster_spill
 
+                # set sync wakee policy tunable
+                echo 1 > /proc/sys/kernel/sched_prefer_sync_wakee_to_waker
 
                 for devfreq_gov in /sys/class/devfreq/qcom,mincpubw*/governor
                 do
@@ -1907,6 +1909,9 @@ case "$target" in
         # Enable Adaptive LMK
         echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
         echo 81250 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
+
+        # set sync wakee policy tunable
+        echo 1 > /proc/sys/kernel/sched_prefer_sync_wakee_to_waker
         # configure governor settings for little cluster
         echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
         echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load

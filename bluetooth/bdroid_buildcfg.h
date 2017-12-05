001da013678741b325b9a15265d47cc5f070f924
Billchen1977 <billchen1977@gmail.com>
2017-12-05 22:05:11 +0530

    tb8703: Add btsco playback
    Change-Id: I8c24fda21ee4fc49f1edfb64746172638353a23e

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,11 +21,13 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-#define BTM_DEF_LOCAL_NAME   "Lenovo TAB3  8 Plus"
+#define BTM_DEF_LOCAL_NAME   "Lenovo TAB3 8 Plus"
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    20
 #define BLE_VND_INCLUDED   TRUE
+#define BLUETOOTH_QTI_SW               TRUE
+#define BT_CLEAN_TURN_ON_DISABLED      TRUE
 // skips conn update at conn completion
 #define BTM_SCO_ENHANCED_SYNC_ENABLED  FALSE
 #endif

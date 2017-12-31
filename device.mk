8ee22f254b4d7d319eb413e5744c13cf21d659a9
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-31 04:40:40 +0200

    tb8703: Build QTI HIDL power 1.1 HAL
    Change-Id: Idb46d7e758bdabd2e30d58e6db6f09ba375c6da4

--- device.mk
+++ device.mk
@@ -255,11 +255,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    power.msm8953
-
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
+    android.hardware.power@1.1-service-qti
 
 # Ramdisk
 PRODUCT_PACKAGES += \

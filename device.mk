afe15803c301124adc55f13eccb28df608cb10ed
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-08 19:21:59 -0800

    tb8703: Build RCS and telephony-ext
    Change-Id: I456aa2db554647f4f6303964f7fb071f98b923ac

--- device.mk
+++ device.mk
@@ -278,6 +278,13 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc
 
+# RCS
+PRODUCT_PACKAGES += \
+    rcs_service_aidl \
+    rcs_service_aidl.xml \
+    rcs_service_api \
+    rcs_service_api.xml
+
 # RIL
 PRODUCT_PACKAGES += \
     libcnefeatureconfig \
@@ -305,6 +312,13 @@ PRODUCT_COPY_FILES += \
 # Shim
 PRODUCT_PACKAGES += \
     libshims_camera
+
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
 
 # Vibrator
 PRODUCT_PACKAGES += \

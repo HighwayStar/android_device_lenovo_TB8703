852f98c436adaef3eee63759c67ce8732d508361
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-05-24 16:05:33 +0800

    tb8703: Ipmport caf lights code, build it

--- device.mk
+++ device.mk
@@ -107,6 +107,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
 
+# Lights
+PRODUCT_PACKAGES += \
+    lights.msm8953
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \

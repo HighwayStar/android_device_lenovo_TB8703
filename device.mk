d70f24926d261f1d0ea059668877a541699ac138
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-05-25 23:53:11 +0800

    tb8703: Add overlays to match stock config

--- device.mk
+++ device.mk
@@ -14,6 +14,11 @@
 # limitations under the License.
 #
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
 # Screen density
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_AAPT_CONFIG := normal

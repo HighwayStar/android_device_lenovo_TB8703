af38019f909cb0d2967cdf8226253a5dcd2cb12c
dianlujitao <dianlujitao@lineageos.org>
2018-11-10 15:11:10 +0800

    tb8703: Update GPS blobs from daisy
     * From: xiaomi/daisy/daisy_sprout:9/PKQ1.180917.001/V10.0.1.0.PDLMIFJ:user/release-keys
    
    Change-Id: Ia1646ae0b8158c1b312221788c08427a754f3a36

--- device.mk
+++ device.mk
@@ -136,7 +136,8 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     libgnss \
-    libgnsspps
+    libgnsspps \
+    libsensorndkbridge
 
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl-qti \

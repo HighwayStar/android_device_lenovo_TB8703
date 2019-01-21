8e3b6ef7477b87424cec02223095e8e6e04e87d8
Bruno Martins <bgcngm@gmail.com>
2019-01-21 09:40:51 +0000

    tb8703: Migrate to livedisplay 2.0
    Change-Id: I513c284c534ce3abc27bf374513280171454f4d1

--- device.mk
+++ device.mk
@@ -110,10 +110,6 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service
 
-# Display calibration
-PRODUCT_PACKAGES += \
-    libjni_livedisplay
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
@@ -188,9 +184,9 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
 
-# LiveDisplay native
+# LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
